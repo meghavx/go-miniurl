b5a88b0aa241452d8b3f4119ba8c4de71a51ad6f
Megha <meghavx@outlook.com>
2025-11-27 22:22:45 +0530

    Add chi router and middleware

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"log"
 	"net/http" // used to create http clients and servers
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	// initialize a new chi router
+	// --methods starting with new are usually constructors
+	router := chi.NewRouter()
+
+	// To print request logs on terminal
+	router.Use(middleware.Logger)
+
+	// register a GET route for the root ("/") path
+	router.Get("/", basicHandler)
 
 	// http server instantiation
 	server := &http.Server{
 		// server properties
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	// run the server
