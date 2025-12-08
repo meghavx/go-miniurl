8545a1231d7d224c0de8cd7b1fc60452fb732ade
Megha <meghavx@outlook.com>
2025-12-09 02:18:38 +0530

    Refactor handlers code

--- internal/handlers.go
+++ internal/handlers.go
@@ -1,8 +1,8 @@
 package internal
 
 import (
+	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -17,27 +17,19 @@ import (
 
 func ShortenURL(w http.ResponseWriter, r *http.Request, db *sql.DB, rdb *redis.Client) {
 	ctx := r.Context()
+	var id int64
 
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
+	longURL, provided := ParseAndGetURL(w, r)
+	if !provided {
 		return
 	}
-
-	longURL := strings.TrimSpace(r.FormValue("url"))
-	if longURL == "" {
-		http.Error(w, "URL required", http.StatusBadRequest)
-		return
-	}
-
 	if !utils.IsValidURL(longURL) {
 		http.Error(w, "Invalid or unsafe URL", http.StatusBadRequest)
 		return
 	}
 
-	var id int64
-	longKey := "long_to_id:" + longURL
-
 	// Try Redis
+	longKey := "long_to_id:" + longURL
 	if cachedID, err := rdb.Get(ctx, longKey).Result(); err == nil {
 		id, _ = strconv.ParseInt(cachedID, 10, 64)
 		code := utils.Base62Encode(uint64(id))
@@ -45,99 +37,94 @@ func ShortenURL(w http.ResponseWriter, r *http.Request, db *sql.DB, rdb *redis.C
 		return
 	}
 
-	// Try SQLite
-	if err := db.QueryRow("SELECT id FROM urls WHERE long_url = ?", longURL).Scan(&id); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			res, err := db.Exec("INSERT INTO urls(long_url) VALUES(?)", longURL)
-			if err != nil {
-				http.Error(w, "Database error", http.StatusInternalServerError)
-				return
-			}
-			id, _ = res.LastInsertId()
-		} else {
-			http.Error(w, "Database error", http.StatusInternalServerError)
-			return
-		}
+	// Redis miss -> Try SQLite
+	if err := db.QueryRow("SELECT id FROM urls WHERE long_url = ?", longURL).Scan(&id); err == nil && id > 0 {
+		code := utils.Base62Encode(uint64(id))
+		writeShortURL(w, r, code)
+		return
 	}
 
-	// Encode id to a short code
-	code := utils.Base62Encode(uint64(id))
-	shortKey := "code_to_long:" + code
-
-	// Set ttl to 24 hours
-	ttl := 24 * time.Hour
-
-	// Write both mappings to redis cache
-	_ = rdb.Set(ctx, shortKey, longURL, ttl).Err()
-	_ = rdb.Set(ctx, longKey, fmt.Sprint(id), ttl).Err()
-
-	// Write response to html template
-	writeShortURL(w, r, code)
+	// SQLite miss ~ New URL -> Insert in DB
+	if res, err := db.Exec("INSERT INTO urls(long_url) VALUES(?)", longURL); err == nil {
+		id, _ = res.LastInsertId()
+		code := utils.Base62Encode(uint64(id))
+		storeInRedis(rdb, ctx, code, longURL, &id)
+		writeShortURL(w, r, code)
+	} else {
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func RedirectURL(w http.ResponseWriter, r *http.Request, code string, db *sql.DB, rdb *redis.Client) {
-	ctx := r.Context()
-	var longURL string
-	key := "code_to_long:" + code
-
-	// Try Redis
-	if longURL, err := rdb.Get(ctx, key).Result(); err == nil {
+	if longURL, found := retrieveLongURL(w, r, db, rdb, code); found {
+		ctx := r.Context()
+		storeInRedis(rdb, ctx, code, longURL, nil)
 		http.Redirect(w, r, longURL, http.StatusMovedPermanently)
-		return
 	}
-
-	// Try SQLite
-	id := utils.Base62Decode(code)
-	if db.QueryRowContext(ctx, "SELECT long_url FROM urls WHERE id = ?", id).Scan(&longURL) != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Cache and redirect
-	_ = rdb.Set(ctx, key, longURL, 0).Err()
-	http.Redirect(w, r, longURL, http.StatusMovedPermanently)
 }
 
 func PreviewURL(w http.ResponseWriter, r *http.Request, db *sql.DB, rdb *redis.Client) {
-	ctx := r.Context()
-
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
+	shortURL, provided := ParseAndGetURL(w, r)
+	if !provided {
 		return
 	}
-
-	shortURL := strings.TrimSpace(r.FormValue("url"))
-	if shortURL == "" {
-		http.Error(w, "URL required", http.StatusBadRequest)
-		return
-	}
-
 	code, err := utils.ExtractShortCode(shortURL)
 	if err != nil {
 		http.Error(w, "Invalid short URL", http.StatusBadRequest)
 		return
 	}
+	if longURL, found := retrieveLongURL(w, r, db, rdb, code); found {
+		writeURLToTemplate(w, "LongURL", longURL, "preview_result.html")
+	}
+}
 
+/**** Helper Methods below ****/
+
+func ParseAndGetURL(w http.ResponseWriter, r *http.Request) (string, bool) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return "", false
+	}
+	url := strings.TrimSpace(r.FormValue("url"))
+	if url == "" {
+		http.Error(w, "URL required", http.StatusBadRequest)
+		return "", false
+	}
+	return url, true
+}
+
+func retrieveLongURL(w http.ResponseWriter, r *http.Request, db *sql.DB, rdb *redis.Client, code string) (string, bool) {
+	ctx := r.Context()
 	var longURL string
-	key := "code_to_long:" + code
 
 	// Try Redis
+	key := "code_to_long:" + code
 	if longURL, err := rdb.Get(ctx, key).Result(); err == nil {
-		writeLongURL(w, longURL)
-		return
+		return longURL, true
 	}
 
-	// Try SQLite
+	// Redis miss -> Try SQLite
 	id := utils.Base62Decode(code)
 	if db.QueryRowContext(ctx, "SELECT long_url FROM urls WHERE id = ?", id).Scan(&longURL) != nil {
 		http.NotFound(w, r)
-		return
+		return "", false
 	}
-	fmt.Println("SQLite miss")
-	fmt.Println("LongURL:", longURL)
+	return longURL, true
+}
 
-	// Write response to html template
-	writeLongURL(w, longURL)
+func storeInRedis(rdb *redis.Client, ctx context.Context, code string, longURL string, id *int64) {
+	ttl := 24 * time.Hour
+
+	// Store code -> longURL mapping
+	shortKey := "code_to_long:" + code
+	_ = rdb.Set(ctx, shortKey, longURL, ttl).Err()
+
+	// Store longURL -> id mapping
+	if id != nil {
+		longKey := "long_to_id:" + longURL
+		_ = rdb.Set(ctx, longKey, fmt.Sprint(id), ttl).Err()
+	}
 }
 
 func writeShortURL(w http.ResponseWriter, r *http.Request, code string) {
@@ -146,17 +133,13 @@ func writeShortURL(w http.ResponseWriter, r *http.Request, code string) {
 		protocol = "http"
 	}
 	shortURL := fmt.Sprintf("%s://%s/%s", protocol, r.Host, code)
-	tmpl := template.Must(template.ParseFiles("static/partials/shorten_result.html"))
-	w.WriteHeader(http.StatusCreated)
-	_ = tmpl.Execute(w, map[string]string{
-		"ShortURL": shortURL,
-	})
+	writeURLToTemplate(w, "ShortURL", shortURL, "shorten_result.html")
 }
 
-func writeLongURL(w http.ResponseWriter, longURL string) {
-	tmpl := template.Must(template.ParseFiles("static/partials/preview_result.html"))
+func writeURLToTemplate(w http.ResponseWriter, key string, val string, tmplName string) {
+	tmpl := template.Must(template.ParseFiles("static/partials/" + tmplName))
 	w.WriteHeader(http.StatusCreated)
 	_ = tmpl.Execute(w, map[string]string{
-		"LongURL": longURL,
+		key: val,
 	})
 }
