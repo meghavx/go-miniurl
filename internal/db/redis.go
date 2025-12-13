db9ea09fe3a743d32c5552bf471cbd777c831647
Megha <meghavx@outlook.com>
2025-12-13 18:05:20 +0530

    make Redis addr configurable via env var

--- internal/db/redis.go
+++ internal/db/redis.go
@@ -1,8 +1,22 @@
 package db
 
-import "github.com/redis/go-redis/v9"
+import (
+	"log"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func InitRedis() *redis.Client {
+	if url := os.Getenv("REDIS_URL"); url != "" {
+		opt, err := redis.ParseURL(url)
+		if err != nil {
+			log.Fatal("Invalid REDIS_URL", err)
+		}
+		return redis.NewClient(opt)
+	}
+
+	// fallback for local dev
 	return redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
